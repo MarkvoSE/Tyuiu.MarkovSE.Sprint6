58039de46de833de99f50601d496384a18d3a870
BerezkinArtem <berezkin.artem2006@gmail.com>
2025-10-22 00:00:32 +0500

    lf

---
+++ DataService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.MarkovSE.Sprint6.Task6.V8.Lib
+{
+    public class DataService : ISprint6Task6V8
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string result = string.Empty;
+
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+
+                string pattern = @"\b\w*z\w*\b";
+                MatchCollection matches = Regex.Matches(content, pattern);
+
+                foreach (Match match in matches)
+                {
+                    result += match.Value + " ";
+                }
+            }
+
+            return result.Trim();
+        }
+    }
+}
---
+++ Tyuiu.MarkovSE.Sprint6.Task6.V8.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+using System.Text.RegularExpressions;
+
+namespace Tyuiu.MarkovSE.Sprint6.Task6.V8.Lib
+{
+    public class DataService : ISprint6Task6V8
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string result = string.Empty;
+
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+
+                string pattern = @"\b\w*z\w*\b";
+                MatchCollection matches = Regex.Matches(content, pattern);
+
+                foreach (Match match in matches)
+                {
+                    result += match.Value + " ";
+                }
+            }
+
+            return result.Trim();
+        }
+    }
+}
