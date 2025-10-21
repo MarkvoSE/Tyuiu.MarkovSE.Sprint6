5dd08cf9f95dc39725f2cf94bde85b18fd15f174
BerezkinArtem <berezkin.artem2006@gmail.com>
2025-10-21 22:53:47 +0500

    s

---
+++ Tyuiu.MarkovSE.Sprint6.Task0.V2/FormMain.cs
@@ -0,0 +1,39 @@
+using Tyuiu.MarkovSE.Sprint6.Task0.V2.Lib;
+namespace Tyuiu.MarkovSE.Sprint6.Task0.V2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonDone_KAA_Click(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+            try
+            {
+                textBoxResult_KAA.Text = Convert.ToString(ds.Calculate(Convert.ToInt32(textBoxInput_KAA.Text)));
+            }
+            catch
+            {
+
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBoxInput_KAA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != ',') && (e.KeyChar != '.') && (e.KeyChar != 8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void buttonHelp_KAA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 0 выполнил студент группы ИИПб-23-1 Карпов Андрей Андреевич", "Сообщение");
+        }
+    }
+}
